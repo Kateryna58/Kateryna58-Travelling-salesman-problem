2290a49ca0200e6ecf15e65da5c87c3a709452b5
Kateryna58 <stadnyk_ak16@nuwm.edu.ua>
2020-10-07 01:34:35 +0300

    Calculated distance between points

--- app.js
+++ app.js
@@ -1,42 +1,3 @@
-// var i,
-//     s,
-//     N = document.querySelector('#cities').value,
-//     E = 500,
-//     g = {
-//         nodes: [],
-//         edges: []
-//     };
-
-// // Generate a random graph:
-// for (i = 0; i < N; i++)
-//     g.nodes.push({
-//         id: 'n' + i,
-//         x: Math.random(),
-//         y: Math.random(),
-//         size: Math.random(),
-//         color: 'rgb(224, 42, 42)'
-//     });
-
-// for (i = 0; i < E; i++)
-//     g.edges.push({
-//         id: 'e' + i,
-//         source: 'n' + (Math.random() * N | 0),
-//         target: 'n' + (Math.random() * N | 0),
-//         size: Math.random(),
-//         color: '#ccc'
-//     });
-
-// function new_graph() {
-//     // Instantiate sigma:
-//     s = new sigma({
-//         graph: g,
-//         container: 'graph-container'
-//     });
-//     console.log(document.querySelector('#cities').value);
-// }
-
-
-
 function getRandomPos(minX, maxX) {
     return Math.random() * (maxX - minX) + minX;
 }
@@ -44,14 +5,12 @@ function getRandomPos(minX, maxX) {
 function createGraph() {
     var e = document.querySelector('#cities'),
         N = e.options[e.selectedIndex].text;
-
     var points = new Array();
     var n = N,
         m = 2;
     for (let i = 0; i < n; i++) {
         points[i] = new Array(m);
     }
-
 
     document.querySelector('#graph-container').innerHTML = "";
     for (i = 0; i < n; i++) {
@@ -65,15 +24,32 @@ function createGraph() {
             }
         }
 
-
-
-        document.querySelector('#graph-container').innerHTML += "<div class='point p" + i + "' style='top:" + Y + "px;left:" + X + "px;'>" + (i + 1) + "</div>";
+        document.querySelector('#graph-container').innerHTML += "<div class='point p" + i + "' style='top:" + Y + "px;left:" + X + "px;'>" + (i) + "</div>";
     }
+    var pairs = new Array();
+    var k = 0;
     for (let i = 0; i < N; i++) {
         for (let j = i + 1; j < N; j++) {
-            document.querySelector('#graph-container').innerHTML += "<svg><line stroke-width='2px' stroke='rgb(219, 162, 88)'  x1=" + points[i][0] + "px y1=" + points[i][1] + "px x2=" + points[j][0] + "px y2=" + points[j][1] + "px /></svg>";
+            document.querySelector('#graph-container').innerHTML += "<svg><line stroke-width='1px' stroke='#000'  x1=" + points[i][0] + "px y1=" + points[i][1] + "px x2=" + points[j][0] + "px y2=" + points[j][1] + "px /></svg>";
+            pairs[k] = [i, j];
+            k++;
         }
+
     }
-
-
+    // console.log(points);
+    var length = new Array();
+    // length = Math.sqrt(Math.pow(points[1][0] - points[0][0], 2) + Math.pow(points[1][1] - points[0][1], 2));
+    // console.log(points[pairs[0][0]][pairs[0][0]]);
+    // console.log(points[0][0]);
+    var x1, x0, y1, y0;
+    for (let i = 0; i < pairs.length; i++) {
+        // x1 = points[pairs[i][i++]][0];
+        // x0 = points[pairs[i][i]][0];
+        // y1 = points[pairs[i][i++]][1];
+        // y0 = points[pairs[i][i]][1];
+        length[i] = Math.sqrt(Math.pow(points[pairs[i][1]][0] - points[pairs[i][0]][0], 2) + Math.pow(points[pairs[i][1]][1] - points[pairs[i][0]][1], 2));
+    }
+    console.log(length);
+    console.log(pairs);
+    console.log(points);
 }

fc71b72f11329626a7cd3d1c5193414a5cb33de7
Kateryna58 <stadnyk_ak16@nuwm.edu.ua>
2020-10-14 02:03:42 +0300

    Added function finding good road

--- app.js
+++ app.js
@@ -38,13 +38,53 @@ function createGraph() {
     }
     var len = new Array();
     for (let i = 0; i < pairs.length; i++) {
-        len[i] = [Math.sqrt(Math.pow(points[pairs[i][1]][0] - points[pairs[i][0]][0], 2) + Math.pow(points[pairs[i][1]][1] - points[pairs[i][0]][1], 2)), ];
-    }
-    for (let i = 0; i < len.length; i++) {
-        len[i][1] = 1 / len[i][0];
+        len[i] = [Math.sqrt(Math.pow(points[pairs[i][1]][0] - points[pairs[i][0]][0], 2) + Math.pow(points[pairs[i][1]][1] - points[pairs[i][0]][1], 2)), ]; //ADDED LENGTH
     }
 
-    console.log(len);
-    // console.log(pairs);
-    // console.log(points);
+    var summ = 0,
+        ALPHA = 1,
+        BETA = 5,
+        RHO = 0.5,
+        Q = 100;
+    for (let i = 0; i < len.length; i++) {
+        len[i][1] = 1 / len[i][0]; //n(r,u)
+        len[i][2] = 0.5; //pheromote t0
+    }
+
+    console.log("LEN(length,n(r,u),pheromone t0):\n", len);
+    console.log("pairs:\n", pairs);
+    console.log("points:\n", points);
+
+
+
+    var curCity = 0;
+    P = new Array();
+    for (let i = 0; i < pairs.length; i++) {
+        if (pairs[i][0] == curCity || pairs[i][1] == curCity) {
+            summ += Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA);
+        }
+    }
+    console.log("summ:\n", summ);
+    var j = 0;
+    for (let i = 0; i < pairs.length; i++) {
+        if (pairs[i][0] == curCity || pairs[i][1] == curCity) {
+            P[j] = (Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA)) / summ;
+            j++;
+        }
+    }
+
+    function min(obj) {
+        var a = obj[0];
+        for (var i = 1; i < obj.length; i++) {
+            if (obj[i] < a) {
+                a = obj[i];
+            }
+        }
+        return a;
+    }
+
+    console.log("P:\n", min(P));
+    console.log("P:\n", P);
+    // console.log("P:\n", P.indexOf(min(P)));
+
 }

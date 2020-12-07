02d321749e6ca8d192f1e551a251f59b042a0f66
Kateryna58 <stadnyk_ak16@nuwm.edu.ua>
2020-12-07 22:03:16 +0200

    add search from point to point

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ function getRandomPos(minX, maxX) {
 }
 
 function createGraph() {
-    var e = document.querySelector('#cities'),
+    let e = document.querySelector('#cities'),
         N = e.options[e.selectedIndex].text;
-    var points = new Array();
-    var n = N,
+        let points = new Array();
+        let n = N,
         m = 2;
     for (let i = 0; i < n; i++) {
         points[i] = new Array(m);
@@ -14,7 +14,7 @@ function createGraph() {
 
     document.querySelector('#graph-container').innerHTML = "";
     for (i = 0; i < n; i++) {
-        var X = getRandomPos(5, window.innerWidth * 0.7 - 50),
+        let X = getRandomPos(5, window.innerWidth * 0.7 - 50),
             Y = getRandomPos(5, window.innerHeight - 50);
         for (j = 0; j < m; j++) {
             if (j == 0) {
@@ -26,8 +26,8 @@ function createGraph() {
 
         document.querySelector('#graph-container').innerHTML += "<div class='point p" + i + "' style='top:" + Y + "px;left:" + X + "px;'>" + (i) + "</div>";
     }
-    var pairs = new Array();
-    var k = 0;
+    let pairs = new Array();
+    let k = 0;
     for (let i = 0; i < N; i++) {
         for (let j = i + 1; j < N; j++) {
             document.querySelector('#graph-container').innerHTML += "<svg><line stroke-width='1px' stroke='#000'  x1=" + points[i][0] + "px y1=" + points[i][1] + "px x2=" + points[j][0] + "px y2=" + points[j][1] + "px /></svg>";
@@ -36,61 +36,78 @@ function createGraph() {
         }
 
     }
-    var len = new Array();
+    let len = new Array();
     for (let i = 0; i < pairs.length; i++) {
         len[i] = [Math.sqrt(Math.pow(points[pairs[i][1]][0] - points[pairs[i][0]][0], 2) + Math.pow(points[pairs[i][1]][1] - points[pairs[i][0]][1], 2)), ]; //ADDED LENGTH
     }
 
-    var summ = 0,
+    let summ = 0,
         ALPHA = 1,
         BETA = 5,
         RHO = 0.5,
         Q = 100;
     for (let i = 0; i < len.length; i++) {
         len[i][1] = 1 / len[i][0]; //n(r,u)
-        len[i][2] = 0.5; //pheromote t0
+        len[i][2] = Q / N; //pheromote t0
     }
 
-    console.log("LEN(length,n(r,u),pheromone t0):\n", len);
-    console.log("pairs:\n", pairs);
-    console.log("points:\n", points);
+    // console.log("LEN(length,n(r,u),pheromone t0):\n", len);
+    // console.log("pairs:\n", pairs);
+    // console.log("points:\n", points);
 
+    function findTheBestRoad() {
+        let tabu = new Array();
+        let indexGoodRoad, P = new Array();
+        let curCity = 0,
+            count = 0;
+        tabu[count] = curCity;
 
+        while (curCity!= N-1 ) {
+            P = [], summ = 0;
+            for (let i = 0; i < pairs.length; i++) {
+                if  ((pairs[i].includes(curCity))&&!(tabu.includes(pairs[i][0])&&(pairs[i][0] != curCity))&&!(tabu.includes(pairs[i][1])&&(pairs[i][1] != curCity))) {
+                    summ += Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA);
 
-    var curCity = 0;
-    P = new Array();
-    for (let i = 0; i < pairs.length; i++) {
-        if (pairs[i][0] == curCity || pairs[i][1] == curCity) {
-            summ += Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA);
-        }
-    }
-    console.log("summ:\n", summ);
-    var j = 0;
-    for (let i = 0; i < pairs.length; i++) {
-        if (pairs[i][0] == curCity || pairs[i][1] == curCity) {
-            P[j] = [(Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA)) / summ, pairs[i][0], pairs[i][1]];
-            j++;
-        }
-    }
-
-    function min(obj) {
-        var a = obj[0];
-        for (var i = 1; i < obj.length; i++) {
-            if (obj[i] > a) {
-                a = obj[i];
+                }
             }
+            console.log("proverka:\n", pairs[0].includes(curCity));
+            console.log("carent city:\n", curCity);
+
+            let j = 0;
+            for (let i = 0; i < pairs.length; i++) {
+                if  ((pairs[i].includes(curCity))&&!(tabu.includes(pairs[i][0])&&(pairs[i][0] != curCity))&&!(tabu.includes(pairs[i][1])&&(pairs[i][1] != curCity))) {
+                    P[j] = [(Math.pow(len[i][2], ALPHA) * Math.pow(len[i][1], BETA)) / summ, pairs[i][0], pairs[i][1]];
+                    j++;
+                    
+                    console.log("pairs mine:\n", pairs[i][0],"hhh",pairs[i][1]);
+                }
+            }
+
+            function max(obj) {
+                let a = obj[0];
+                for (let i = 1; i < obj.length; i++) {
+                    if (obj[i] > a) {
+                        a = obj[i];
+                    }
+                }
+                return a;
+            }
+
+            indexGoodRoad = P.indexOf(max(P));
+           
+            console.log("P[indexGoodRoad]",P[indexGoodRoad]);
+            for (let i = 1; i < 3; i++) {
+                if(P[indexGoodRoad][i]!=curCity){
+                    curCity = P[indexGoodRoad][i];
+                    count++;
+                    tabu[count] = P[indexGoodRoad][i];
+                    break;
+                }
+            }
+            document.querySelector('#graph-container').innerHTML += "<svg><line stroke-width='2px' stroke='rgb(248, 58, 58)'  x1=" + points[P[indexGoodRoad][1]][0] + "px y1=" + points[P[indexGoodRoad][1]][1] + "px x2=" + points[P[indexGoodRoad][2]][0] + "px y2=" + points[P[indexGoodRoad][2]][1] + "px /></svg>";
         }
-        return a;
+        console.log("tabu",tabu);
+
     }
-    var indexGoodRoad = P.indexOf(min(P));
-    document.querySelector('#graph-container').innerHTML += "<svg><line stroke-width='2px' stroke='rgb(248, 58, 58)'  x1=" + points[P[indexGoodRoad][1]][0] + "px y1=" + points[P[indexGoodRoad][1]][1] + "px x2=" + points[P[indexGoodRoad][2]][0] + "px y2=" + points[P[indexGoodRoad][2]][1] + "px /></svg>";
-
-
-
-
-
-    console.log("P:\n", P.indexOf(min(P)));
-    console.log("P:\n", min(P));
-    console.log("P:\n", P);
-    console.log("P:\n", P[indexGoodRoad][1]);
+    findTheBestRoad();
 }
